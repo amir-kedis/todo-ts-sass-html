ce42bde447601ffc757a594ac973bf449ff66d5c
amir-kedis <amir.kedis@gmail.com>
2023-10-22 09:08:30 +0300

    chore: rename some files for better conventions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
-// import "@fortawsome/fontawesome-free/css/solid.css";
-import "@fortawesome/fontawesome-free/css/brands.css";
 
 function main() { }
 
