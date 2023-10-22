10f1de702fc8ee7adab57e884465a31baae8440a
amir-kedis <amir.kedis@gmail.com>
2023-10-22 11:25:49 +0300

    feat: task class

---
+++ src/classes/Task.ts
@@ -0,0 +1,57 @@
+type TaskObject = {
+  name: string;
+  description?: string;
+  completed?: boolean;
+  dueDate?: Date;
+  priority?: "no" | "low" | "medium" | "high";
+};
+
+export default class Task {
+  name: string;
+  description?: string;
+  completed?: boolean;
+  dueDate?: Date;
+  priority?: "no" | "low" | "medium" | "high";
+
+  constructor(info: TaskObject) {
+    console.log("Task constructor called");
+    console.log(info);
+    this.name = info.name;
+    this.description = info.description;
+    this.completed = info.completed;
+    this.dueDate = info.dueDate;
+    this.priority = info.priority;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setDescription(description: string) {
+    this.description = description;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  setCompleted(completed: boolean) {
+    this.completed = completed;
+  }
+
+  getCompleted() {
+    return this.completed;
+  }
+
+  setDueDate(dueDate: Date) {
+    this.dueDate = dueDate;
+  }
+
+  getDueDate() {
+    return this.dueDate;
+  }
+}
